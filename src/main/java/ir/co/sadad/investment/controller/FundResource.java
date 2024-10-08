e50844b150e6bc16bd54c0c7bb606b6b14293570
Ghazaleh Shahrokhabadi <gh.nshahrokhi@gmail.com>
2024-10-08 13:42:05 +0330

    Update FundResource.java

--- src/main/java/ir/co/sadad/investment/controller/FundResource.java
+++ src/main/java/ir/co/sadad/investment/controller/FundResource.java
@@ -1,24 +1,4 @@
-package ir.co.sadad.investment.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import ir.co.sadad.investment.dto.FundInfoResponseDto;
-import ir.co.sadad.investment.dto.FundResponseDto;
-import ir.co.sadad.investment.services.CustomerFundService;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static ir.co.sadad.investment.common.Constants.SSN;
 
 @RestController
 @RequiredArgsConstructor
