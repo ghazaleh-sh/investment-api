eaddd3c818de9a85c65e18022c72ca4f0f93cc0d
Ghazaleh Shahrokhabadi <gh.nshahrokhi@gmail.com>
2024-10-08 13:41:43 +0330

    Update IssueResource.java

--- src/main/java/ir/co/sadad/investment/controller/IssueResource.java
+++ src/main/java/ir/co/sadad/investment/controller/IssueResource.java
@@ -1,22 +1,4 @@
-package ir.co.sadad.investment.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import ir.co.sadad.investment.dto.IssueReqDto;
-import ir.co.sadad.investment.dto.IssueResDto;
-import ir.co.sadad.investment.services.InvestmentJobService;
-import ir.co.sadad.investment.services.IssueService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import static ir.co.sadad.investment.common.Constants.SSN;
 
 @RestController
 @RequiredArgsConstructor
