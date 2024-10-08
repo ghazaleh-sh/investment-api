88c3a55289e1a6b2f7ac81e3d3e00d3e643b1771
Ghazaleh Shahrokhabadi <gh.nshahrokhi@gmail.com>
2024-10-08 13:41:30 +0330

    Update IssueResource.java

--- src/main/java/ir/co/sadad/investment/controller/IssueResource.java
+++ src/main/java/ir/co/sadad/investment/controller/IssueResource.java
@@ -19,40 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import static ir.co.sadad.investment.common.Constants.SSN;
 
 @RestController
-@RequestMapping(path = "${v1API}/issue")
-@Tag(description = "سرویس های صدور", name = "issue services")
 @RequiredArgsConstructor
 public class IssueResource {
-    private final IssueService issueService;
-    private final InvestmentJobService jobService;
-
-
-    @Operation(summary = "سرویس درخواست صدور")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = IssueResDto.class)))
-    @PostMapping("/create")
-    public ResponseEntity<IssueResDto> createIssue(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authToken,
-                                                   @RequestHeader(SSN) String ssn,
-                                                   @Valid @RequestBody IssueReqDto issueReqDto,
-                                                   @RequestHeader(name = "otp", required = false) String authorizationCode) {
-        return new ResponseEntity<>(issueService.createInitialIssue(issueReqDto, authToken, ssn, authorizationCode), HttpStatus.OK);
-    }
-
-    @Operation(summary = "سرویس لغو صدور")
-    @ApiResponse(responseCode = "204")
-    @PostMapping("/cancel")
-    public ResponseEntity<HttpStatus> cancelIssue(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authToken,
-                                                  @RequestHeader(SSN) String ssn,
-                                                  @RequestHeader(name = "otp", required = false) String otp,
-                                                  @RequestParam(name = "uuid") String uuid,
-                                                  @RequestParam(name = "fundId") String fundId) {
-        issueService.cancelIssue(uuid, fundId, ssn, otp);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @Operation(summary = "صدور/ابطال از طریق جاب")
-    @GetMapping("/job")
-    public ResponseEntity<Object> investmentByJob() {
-        jobService.issueRevokeJobService();
-        return ResponseEntity.ok().build();
-    }
+    
 }
