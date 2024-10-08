0f18e976ec4fe0b67b4a50e9e73f7fd86b2283c6
Ghazaleh Shahrokhabadi <gh.nshahrokhi@gmail.com>
2024-10-08 13:41:08 +0330

    Update FundResource.java

--- src/main/java/ir/co/sadad/investment/controller/FundResource.java
+++ src/main/java/ir/co/sadad/investment/controller/FundResource.java
@@ -21,28 +21,7 @@ import java.util.List;
 import static ir.co.sadad.investment.common.Constants.SSN;
 
 @RestController
-@RequestMapping(path = "${v1API}/fund")
-@Tag(description = "سرویس های صندوق", name = "Funds services")
 @RequiredArgsConstructor
 public class FundResource {
-    private static final Logger log = LoggerFactory.getLogger(FundResource.class);
-    private final CustomerFundService customerFundService;
-
-    @Operation(summary = "سرویس دریافت لیست صندوق ها ")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = FundResponseDto.class)))
-    @GetMapping("/list")
-    public ResponseEntity<List<FundResponseDto>> add(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authToken,
-                                                     @RequestHeader(name = "X-Gateway-Path",required = false) String baseUrl,
-                                                     @RequestHeader(SSN) String ssn) {
-
-        return new ResponseEntity<>(customerFundService.getCustomerFund(ssn, baseUrl), HttpStatus.OK);
-    }
-
-    @Operation(summary = "سرویس مشخصات صندوق ")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = FundInfoResponseDto.class)))
-    @GetMapping("/info/{fundId}")
-    public ResponseEntity<FundInfoResponseDto> info(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authToken,
-                                                    @PathVariable("fundId") Integer fundId) {
-        return new ResponseEntity<>(customerFundService.getFundInfo(fundId), HttpStatus.OK);
-    }
+  
 }
